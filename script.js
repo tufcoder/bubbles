7d05e1f91f7f75d41707b8940c7d9c05e2253b4b
Oswaldo Castro <oswaldogpc@gmail.com>
2024-03-21 15:51:01 -0300

    update rand background

--- script.js
+++ script.js
@@ -5,61 +5,84 @@ setTimeout(frequency, timer)
 
 function frequency() {
     const screen = container.clientWidth * container.clientHeight
-    if (screen <= 480000) {
+    if (screen <= 480000)
         timer = 1000
-    }
-    else if (screen <= 1024000) {
+    else if (screen <= 1024000)
         timer = 800
-    }
-    else {
+    else
         timer = 100
-    }
     makeBubbles()
     setTimeout(frequency, timer)
 }
 
 const clearBubbles = setInterval(() => {
     const delBubbles = []
-    const clear = container.querySelectorAll('.circle')
-    clear.forEach(item => {
-        if (item.style.opacity == 0)
-            delBubbles.push(item)
+    const bubbles = container.querySelectorAll('.circle')
+    bubbles.forEach(bubble => {
+        if (bubble.style.opacity == 0)
+            delBubbles.push(bubble)
     })
-    while (delBubbles.length > 0) {
+    while (delBubbles.length > 0)
         container.removeChild(delBubbles.shift())
-    }
     container.style.backgroundColor = randColor()
-}, 20000);
+}, 10000)
+
+// container.addEventListener("dragenter", (e) => {
+//     e.preventDefault()
+// })
+
+// container.addEventListener("dragover", (e) => {
+//     e.preventDefault()
+// })
+
+// container.addEventListener("dragleave", (e) => {
+//     e.preventDefault()
+// })
+
+// container.addEventListener("drop", (e) => {
+//     const id = e.dataTransfer.getData('text/plain')
+//     const draggable = document.getElementById(id);
+//     e.target.appendChild(draggable)
+//     draggable.classList.remove('hide')
+// })
 
 function makeBubbles() {
-    const x = Math.floor(Math.random() * container.clientWidth + 1)
-    const y = Math.floor(Math.random() * container.clientHeight + 1)
-    return container.appendChild(createCircle(x, y))
+    return container.appendChild(createCircle())
 }
 
-function createCircle(x, y) {
+function createCircle() {
+    const x = Math.floor(Math.random() * container.clientWidth + 1)
+    const y = Math.floor(Math.random() * container.clientHeight + 1)
     const div = document.createElement('div')
     div.id = 'div_' + x + '_' + y + '_' + Date.now().toString()
-    div.style.width = 0;
-    div.style.height = 0;
-    div.style.left = x + 'px'
+    div.style.width = 0
+    div.style.height = 0
     div.style.top = y + 'px'
+    div.style.left = x + 'px'
+    div.style.borderRadius = '50%'
     div.className = 'circle'
     div.style.backgroundColor = randColor()
+    div.setAttribute('draggable', true)
     div.addEventListener("transitionend", () => {
-        div.style.opacity = 0;
+        div.style.opacity = 0
     })
+    // div.addEventListener("dragstart", (e) => {
+    //     e.dataTransfer.setData('text/plain', e.target.id)
+    //     setTimeout(() => {
+    //         e.target.classList.add('hide')
+    //     }, 0);
+    // })
     setTimeout(() => {
         const radius = randRadius()
         div.style.width = radius + 'px'
         div.style.height = radius + 'px'
         div.style.opacity = randOpacity()
-    }, 100);
+    }, 100)
     return div
 }
 
 function randRadius(max = 500, min = 100) {
-    return Math.floor(Math.random() * max) + min;
+    return Math.floor(Math.random() * max) + min
 }
 
 function randColor() {
